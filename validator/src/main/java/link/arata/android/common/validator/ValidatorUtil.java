19ee1f02c5f69036ee14731ca4b1b647c37d3546
webarata <arata.shinichi@gmail.com>
2016-02-24 18:25:29 +0900

    ValidatorUtilの追加

---
+++ validator/src/main/java/link/arata/android/common/validator/ValidatorUtil.java
@@ -0,0 +1,34 @@
+package link.arata.android.common.validator;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Validatorを使うためのユーティリティ
+ *
+ * @author arata
+ */
+public abstract class ValidatorUtil {
+    /**
+     * textをvalidatorsでチェックする。<br>
+     * 最初に見つかったエラーを返す。<br>
+     * validationにすべて成功した場合にはnullを返す
+     *
+     * @param context    コンテキスト
+     * @param value      検査するテキスト
+     * @param validators textを検査するValidator
+     * @return エラーメッセージ。なければnull
+     */
+    @Nullable
+    public static String validate(@NonNull Context context, @NonNull String value, @NonNull Validator... validators) {
+        String message = null;
+        for (Validator validator : validators) {
+            message = validator.validate(context, value);
+            if (message != null) {
+                break;
+            }
+        }
+        return message;
+    }
+}
