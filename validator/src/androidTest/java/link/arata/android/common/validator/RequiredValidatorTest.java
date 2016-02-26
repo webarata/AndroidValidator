d8160a66bc465b7aae01073735f6ee1fa7b95fed
webarata <arata.shinichi@gmail.com>
2016-02-26 18:42:35 +0900

    テストが動くようにした

--- validator/src/androidTest/java/link/arata/android/common/validator/RequiredValidatorTest.java
+++ validator/src/androidTest/java/link/arata/android/common/validator/RequiredValidatorTest.java
@@ -1,7 +1,15 @@
 package link.arata.android.common.validator;
 
-/**
- * Created by arata on 2016/02/25.
- */
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class RequiredValidatorTest {
+    @Test
+    public void test(){
+        Assert.fail();
+    }
 }
