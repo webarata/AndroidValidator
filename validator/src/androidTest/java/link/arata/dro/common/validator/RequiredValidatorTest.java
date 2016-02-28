e7df7855e325a600263425566946b38079496319
webarata <arata.shinichi@gmail.com>
2016-02-28 16:55:34 +0900

    RequiredValidatorのテストの追加

--- validator/src/androidTest/java/link/arata/dro/common/validator/RequiredValidatorTest.java
+++ validator/src/androidTest/java/link/arata/dro/common/validator/RequiredValidatorTest.java
@@ -1,15 +1,35 @@
 package link.arata.dro.common.validator;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
 @RunWith(AndroidJUnit4.class)
 public class RequiredValidatorTest {
+    private Context context;
+
+    @Before
+    public void setUp() throws Exception {
+        context = InstrumentationRegistry.getContext();
+    }
+
     @Test
-    public void test(){
-        Assert.assertTrue(true);
+    public void requiredValidatorでtestの場合() {
+        Validator validator = new RequiredValidator();
+        assertThat(validator.validate(context, "test"), is(nullValue()));
+    }
+
+    @Test
+    public void requiredValidatorで空文字の場合() {
+        Validator validator = new RequiredValidator();
+        assertThat(validator.validate(context, ""), is(context.getString(R.string.validator_required)));
     }
 }
