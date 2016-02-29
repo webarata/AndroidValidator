eeca0d9b4cb0be539443f8edc209e2c0c8d85bea
webarata <arata.shinichi@gmail.com>
2016-02-29 18:38:59 +0900

    common-utilのバージョンを0.1.2に変更

--- validator/src/androidTest/java/link/arata/dro/common/validator/RequiredValidatorTest.java
+++ validator/src/androidTest/java/link/arata/dro/common/validator/RequiredValidatorTest.java
@@ -8,6 +8,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import link.arata.common.enums.LineBreakType;
+import link.arata.common.enums.TrimType;
+import link.arata.common.helper.ValidationHelper;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -25,19 +29,25 @@ public class RequiredValidatorTest {
 
     @Test
     public void requiredValidatorでtestの場合() {
-        Validator validator = new RequiredValidator();
+        Validator validator = new RequiredValidator(ValidationHelper.getInstance(TrimType.RIGHT, LineBreakType.LF));
         assertThat(validator.validate(context, "test"), is(nullValue()));
     }
 
     @Test
     public void requiredValidatorで空文字の場合() {
-        Validator validator = new RequiredValidator();
+        Validator validator = new RequiredValidator(ValidationHelper.getInstance(TrimType.RIGHT, LineBreakType.LF));
         assertThat(validator.validate(context, ""), is(context.getString(VALIDATOR_MESSAGE_ID)));
     }
 
     @Test
     public void requiredValidatorで空白文字の場合() {
-        Validator validator = new RequiredValidator();
+        Validator validator = new RequiredValidator(ValidationHelper.getInstance(TrimType.RIGHT, LineBreakType.LF));
         assertThat(validator.validate(context, " \r\n\t"), is(context.getString(VALIDATOR_MESSAGE_ID)));
+    }
+
+    @Test
+    public void requiredValidatorでtrimをNONEにして空白文字の場合() {
+        Validator validator = new RequiredValidator(ValidationHelper.getInstance(TrimType.NONE, LineBreakType.LF));
+        assertThat(validator.validate(context, " \r\n\t"), is(nullValue()));
     }
 }
