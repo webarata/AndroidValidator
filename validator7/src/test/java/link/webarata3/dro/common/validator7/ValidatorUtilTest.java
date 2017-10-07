825c93cece11e5d3b1a3404cddd0a1dd4c1c6be7
webarata3 <webarata3@gmail.com>
2017-10-08 01:14:01 +0900

    テストの追加

--- validator7/src/test/java/link/webarata3/dro/common/validator7/ValidatorUtilTest.java
+++ validator7/src/test/java/link/webarata3/dro/common/validator7/ValidatorUtilTest.java
@@ -1,6 +1,7 @@
 package link.webarata3.dro.common.validator7;
 
 import android.content.Context;
+import android.widget.TextView;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import link.webarata3.dro.common.validator7.enums.TrimType;
 import link.webarata3.dro.common.validator7.helper.ValidationHelper;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -20,14 +22,47 @@ public class ValidatorUtilTest {
     @Mock
     private Context mockContext;
 
-    @Test
-    public void test() {
-        final String expectedMessage = "入力してください";
+    @Mock
+    private TextView mockTextView;
 
+    @Test
+    public void requiredValidatorでtestの場合() {
+        Validator validator = new RequiredValidator();
+        ValidationHelper validationHelper = ValidationHelper.getInstance(TrimType.RIGHT, LineBreakType.LF);
+        assertThat(ValidatorUtil.validate(mockContext, validationHelper, "test", validator), is(nullValue()));
+    }
+
+    @Test
+    public void requiredValidatorで空文字の場合() {
+        final String expectedMessage = "入力してください";
         when(mockContext.getString(R.string.validator_required)).thenReturn(expectedMessage);
 
         Validator validator = new RequiredValidator();
         ValidationHelper validationHelper = ValidationHelper.getInstance(TrimType.RIGHT, LineBreakType.LF);
         assertThat(ValidatorUtil.validate(mockContext, validationHelper, "", validator), is(expectedMessage));
+    }
+
+    @Test
+    public void validatorTextViewでtestの場合() {
+        when(mockTextView.getText()).thenReturn("test");
+
+        Validator validator = new RequiredValidator();
+        ValidationHelper validationHelper = ValidationHelper.getInstance(TrimType.RIGHT, LineBreakType.LF);
+        assertThat(ValidatorUtil.validateEditText(mockContext, validationHelper, mockTextView, validator), is(true));
+        assertThat(mockTextView.getError(), is(nullValue()));
+    }
+
+    @Test
+    public void validatorTextViewで空文字の場合() {
+        final String expectedMessage = "入力してください";
+        when(mockContext.getString(R.string.validator_required)).thenReturn(expectedMessage);
+
+        when(mockTextView.getText()).thenReturn("");
+        when(mockTextView.getError()).thenReturn(expectedMessage);
+
+        Validator validator = new RequiredValidator();
+        ValidationHelper validationHelper = ValidationHelper.getInstance(TrimType.RIGHT, LineBreakType.LF);
+        assertThat(ValidatorUtil.validateEditText(mockContext, validationHelper, mockTextView, validator), is(false));
+        assertThat(mockTextView.getError().toString(), is(expectedMessage));
     }
 }
